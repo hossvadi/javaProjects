9ff803ada76ae6bead3f4f3cfb4953c5c1b999e2
hossvadi <hossvadi@gmail.com>
2022-07-25 12:37:12 +0200

    simpleGame

---
+++ simpleGame/simpleGame/src/main/java/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        Player1 player = new Player1("Moein", "knife", 100);
+//        System.out.println(player.getName());
+//        System.out.println(player.getHealth());
+//        System.out.println(player.getWeapon());
+
+//        player.damageByGun1();
+//        player.damageByGun1();
+//        player.damageByGun2();
+//        player.heal();
+
+        Player2 betterPlayer1 = new Player2("Iman", "machine gun", 80, false);
+        betterPlayer1.damageByGun1();
+        betterPlayer1.damageByGun2();
+        betterPlayer1.damageByGun2();
+        betterPlayer1.heal();
+
+//        Player2 betterPlayer2 = new Player2("Mamal", "machine gun", 60, true);
+//        betterPlayer2.damageByGun1();
+//        betterPlayer2.damageByGun2();
+//        betterPlayer2.damageByGun2();
+//        betterPlayer2.heal();
+    }
+}
---
+++ simpleGame/simpleGame/src/main/java/Player2.java
@@ -0,0 +1,51 @@
+public class Player2 extends Player1 {
+    private int health;
+    private final boolean armour;
+
+    public Player2(String name, String weapon, int health, boolean armour) {
+        super(name, weapon, health);
+        this.health = health;
+        this.armour = armour;
+    }
+
+    @Override
+    public void damageByGun1() {
+        if (armour) {
+            this.health -= 20;
+            if(this.health <=0) this.health =0;
+            System.out.println("Armour is on, Got hit by Gun 1. the health reduced by 20. " +
+                    "Current health is " + this.health + ".");
+        } if (!armour) {
+            this.health -= 30;
+            if(this.health <=0) this.health =0;
+            System.out.println("Armour is off, Got hit by Gun 1. the health reduced by 30. " +
+                    "Current health is " + this.health + ".");
+        }
+        if (this.health == 0) {
+            System.out.println(getName() + " is dead!");
+        }
+    }
+
+    @Override
+    public void damageByGun2() {
+        if (armour) {
+            this.health -= 40;
+            if(this.health <=0) this.health =0;
+            System.out.println("Armour is on, Got hit by Gun 1. the health reduced by 40. " +
+                    "Current health is" + this.health);
+        } if (!armour) {
+            this.health -= 50;
+            if(this.health <=0) this.health =0;
+            System.out.println("Armour is off, Got hit by Gun 1. the health reduced by 50." +
+                    "Current health is" + this.health);
+        }
+        if (this.health == 0) {
+            System.out.println(getName() + " is dead!");
+        }
+    }
+
+    @Override
+    public void heal() {
+        super.heal();
+    }
+}
