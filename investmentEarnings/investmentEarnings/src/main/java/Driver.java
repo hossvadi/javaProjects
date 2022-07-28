786588932202b882bf2a167050a10027341de24f
hossvadi <hossvadi@gmail.com>
2022-07-28 07:49:46 +0200

    investmentEarnings

--- investmentEarnings/investmentEarnings/src/main/java/Driver.java
+++ investmentEarnings/investmentEarnings/src/main/java/Driver.java
@@ -1,34 +1,36 @@
-import sun.security.krb5.internal.EncAPRepPart;
-
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your bying price per share");
+        System.out.println("Enter your buying price per share: ");
 
         double buyingPrice = scanner.nextDouble();
         int day = 1;
         double closingPrice = 0.1;
+        DecimalFormat df = new DecimalFormat("0.00");
 
         while (true) {
             System.out.println("Enter the closing price for day "
-                + day + "(any negative value to leave : ");
+                + day + " (any negative value to leave) : ");
             closingPrice = scanner.nextDouble();
             if (closingPrice <  0.0) break;
             double earnings = closingPrice - buyingPrice;
             if (earnings > 0) {
                 System.out.println("After day " + day + ", you earned "
-                    + earnings + "per share. ");
+                    + df.format(earnings) + " per share. ");
             }
             else if (earnings < 0.0) {
-                System.out.println("After day " + day + ", you lost "
-                        + (-earnings) + "per share. ");
+                System.out.println("After day " + day + ", you lost -"
+                        + df.format(-earnings) + " per share. ");
             }
             else {
                 System.out.println("After day " + day + "you have "
-                        + "no earnings per share. ");
+                        + " no earnings per share. ");
             }
+            day += 1;
         }
+        scanner.close();
     }
 }
