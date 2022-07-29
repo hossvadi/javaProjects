55887cd31fb1d995e694cb61e5e778f71d378d14
hossvadi <hossvadi@gmail.com>
2022-07-29 13:30:24 +0200

    budgetAllocationFinalized

--- budgetAllocation/budgetAllocation/src/main/java/Driver.java
+++ budgetAllocation/budgetAllocation/src/main/java/Driver.java
@@ -19,18 +19,20 @@ public class Driver {
             double value = scan.nextDouble();
             proportion.add(value);
             sum += proportion.get(i);
+            i++;
         } while (sum <= 100);
         scan.close();
         if (sum > 100) {
             double cumulativeSum = 0.0;
             for (int j = 0; j < proportion.size()-1; j++) {
-                cumulativeSum =+ proportion.get(j);
+                cumulativeSum += proportion.get(j);
             }
             proportion.set(proportion.size() - 1,
                     100.0 - cumulativeSum);
         }
         for (double value : proportion) {
-
+            double expense = value * total / 100.0;
+            System.out.println("Your " + value + "% equals $ " + expense);
         }
     }
 }
