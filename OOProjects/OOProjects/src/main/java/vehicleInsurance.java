c9e7eeeb94ed5ea5fa5aee5b755bcba4e7415d9f
hossvadi <hossvadi@gmail.com>
2022-08-01 00:26:57 +0200

    vehicleInsurance

---
+++ OOProjects/OOProjects/src/main/java/vehicleInsurance.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class vehicleInsurance {
+    public static void main(String[] args) {
+        System.out.println("Enter your vehicle type: ");
+        System.out.println("Car, truck, Bus, Motorcycle");
+        Scanner scan = new Scanner(System.in);
+        String type = scan.nextLine();
+
+        int premium = 0;
+
+        switch (type) {
+            case "car":
+                premium = 200;
+                break;
+            case "truck":
+                premium = 300;
+                break;
+            case "bus":
+                premium = 400;
+                break;
+            case "motorcycle":
+                System.out.println("what is the type of youe moto? ");
+                System.out.println("1 (low power), 2 (medium), 3 (high)");
+                String motoType = scan.nextLine();
+                int motoTypeInt = Integer.parseInt(motoType);
+                switch (motoTypeInt) {
+                    case 1:
+                        premium = 300;
+                        break;
+                    case 2:
+                        premium = 400;
+                        break;
+                    case 3:
+                        premium = 500;
+                        break;
+                    default:
+                        System.out.println("Unknown Motorcycle type... ");
+                }
+                break;
+            default:
+                System.out.println("Unknown vehicle type... ");
+        }
+        scan.close();
+        System.out.println("The premium is $ " + premium);
+    }
+}
