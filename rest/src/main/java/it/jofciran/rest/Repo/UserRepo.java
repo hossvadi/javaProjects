a91afea3022433c1cced317b051647b7c9d51ac3
hossvadi <hossvadi@gmail.com>
2022-08-03 16:03:46 +0200

    CRUD RESTful API

--- rest/src/main/java/it/jofciran/rest/Repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package it.jofciran.rest.Repo;
-
-import it.jofciran.rest.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-}
