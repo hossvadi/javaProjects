adb466ca09cd9ea7953ee62c4819cae3b06313d9
hossvadi <hossvadi@gmail.com>
2022-08-08 09:43:55 +0200

    rosterTableInit

---
+++ OOProjects/OOProjects/src/main/java/rosterTable.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class rosterTable {
+    public static void main(String[] args) {
+        int days = 3;
+
+        // Arraylist of  ArrayLists
+
+        ArrayList<ArrayList<String>> roster = new ArrayList<ArrayList<String>>(days);
+
+        //each sub-ArrayList
+        ArrayList<String> day1 = new ArrayList<String>();
+        ArrayList<String> day2 = new ArrayList<String>();
+        ArrayList<String> day3 = new ArrayList<String>();
+    }
+}
