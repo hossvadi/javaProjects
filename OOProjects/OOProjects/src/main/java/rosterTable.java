8ff7c774016abce606a8934b4b184acf9243a2b3
hossvadi <hossvadi@gmail.com>
2022-08-08 09:52:11 +0200

    rosterTable

--- OOProjects/OOProjects/src/main/java/rosterTable.java
+++ OOProjects/OOProjects/src/main/java/rosterTable.java
@@ -12,5 +12,21 @@ public class rosterTable {
         ArrayList<String> day1 = new ArrayList<String>();
         ArrayList<String> day2 = new ArrayList<String>();
         ArrayList<String> day3 = new ArrayList<String>();
+        day1.add("Hossein");
+        day1.add("Javadi");
+        roster.add(day1);
+        day2.add("Reza");
+        day2.add("Mohammad");
+        day2.add("Javad");
+        roster.add(day2);
+        day3.add("Ali");
+        day3.add("Taghi");
+        roster.add(day3);
+
+        System.out.println(roster);
+    }
+
+    public static void getDays(ArrayList<ArrayList<String>> rosterTable, String name) {
+        for (ArrayList<String> arrList : )
     }
 }
